d71d63044722dfe6d70e1879c16df4dffc12bc3c
fezzy <fezzy102@gmail.com>
2013-03-14 18:13:02 -0400

    'Updating device tree'

--- aocp.mk
+++ aocp.mk
@@ -6,6 +6,9 @@ $(call inherit-product, vendor/aocp/config/gsm.mk)
 # Inherit some common aocp stuff.
 $(call inherit-product, vendor/aocp/config/common_full_phone.mk)
 
+# Enhanced NFC
+$(call inherit-product, vendor/aocp/config/nfc_enhanced.mk)
+
 PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=d2uc TARGET_DEVICE=d2att BUILD_FINGERPRINT="d2uc-user 4.0.4 IMM76D I747UCALEM release-keys" PRIVATE_BUILD_DESC="samsung/d2uc/d2att:4.0.4/IMM76D/I747UCALEM:user/release-keys"
 
 PRODUCT_NAME := aocp_d2att
