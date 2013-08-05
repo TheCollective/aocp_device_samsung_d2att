9578536d01e2eb53afc97d897daa2fab847e8652
fezzy <fezzy@fezzy-VirtualBox.(none)>
2013-08-05 14:04:56 -0400

    "update device tree to 4.3'

--- aocp.mk
+++ aocp.mk
@@ -9,7 +9,11 @@ $(call inherit-product, vendor/aocp/config/nfc_enhanced.mk)
 # Inherit some common aocp stuff.
 $(call inherit-product, vendor/aocp/config/common_full_phone.mk)
 
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=d2uc TARGET_DEVICE=d2att BUILD_FINGERPRINT="samsung/d2uc/d2att:4.0.4/IMM76D/I747UCALEM:user/release-keys" PRIVATE_BUILD_DESC="d2uc-user 4.0.4 IMM76D I747UCALEM release-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRODUCT_NAME=d2uc \
+    TARGET_DEVICE=d2att \
+    BUILD_FINGERPRINT="samsung/d2uc/d2att:4.1.2/JZO54K/I747UCDMG2:user/release-keys" \
+    PRIVATE_BUILD_DESC="d2uc-user 4.1.2 JZO54K I747UCDMG2 release-keys"
 
 PRODUCT_NAME := aocp_d2att
 PRODUCT_DEVICE := d2att
